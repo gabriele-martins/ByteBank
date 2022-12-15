fcb02f3fd378bbbd21ddc6d40f1dd445fe20d228
Gabriele Martins <gabriele.martinsp@gmail.com>
2022-12-15 20:29:28 -0300

    fist commit

---
+++ ByteBank1/Conexao.cs
@@ -0,0 +1,7 @@
+﻿namespace ByteBank1
+{
+    class Conexao
+    {
+        public static string sqLiteConnection = @"Data Source=BancoDeDados.db; Version=3; Mode=ReadWrite; New=False; Compress=True; Journal Mode=Off;";
+    }
+}
